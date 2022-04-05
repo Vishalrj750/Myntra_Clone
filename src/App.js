57eb6e0537ddeec1f3370df4d675c804bd907aec
suma <21gsuma@gmail.com>
2022-04-05 23:04:05 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Footer from './Components/Footer';
 import { Navbar } from './Components/Navbar';
+import {HomeAll} from './Components/HomeAll'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HomeAll />
+
       <Footer/>
     </div>
   );
