620ca4f3272c5885d1e6c798ff334aaff3e0513a
Vishal Kumar Singh <vishalsingh8574031285@gmail.com>
2022-04-09 10:00:20 +0530

    Minor Changes

--- src/Components/Wishlist.jsx
+++ src/Components/Wishlist.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from 'styled-components';
 import WishlistCard from "./WishlistCard";
 import {useSelector} from "react-redux"
-import { useNavigate } from "react-router-dom";
 
 const WishlistWrapper = styled.div`
 margin-top: 95px;;
@@ -17,10 +16,9 @@ margin-left: 5%;
 
 function Wishlist() {
 
-    const navigate = useNavigate()
+    
 
     const wish=useSelector(state=>state.wishlist)
-    const isAuth = useSelector((state) => state.isAuth)
     return(
         <WishlistWrapper>
             {wish.map((e)=><WishlistCard item={e}/>)}
